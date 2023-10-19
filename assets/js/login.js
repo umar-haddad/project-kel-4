26f93df30045bb1b19deee4045aeac4dcd61bcdb
umar-haddad <sayyidumar11@gmail.com>
2023-10-19 21:57:57 +0700

    perubahan_10/19/23

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,56 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let loginForm = document.querySelector('form');
-    let emailInput = document.querySelector('#email');
-    let passwordInput = document.querySelector('#password');
-    let forgotPasswordLink = document.querySelector('#forgotPasswordLink');
+const form = document.querySelector('form');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const url = 'https://652fea546c756603295ded28.mockapi.io/dataStrotage'
 
-    // Cek apakah pengguna sudah login saat halaman dimuat
-    if (localStorage.getItem('token')) {
-        // Pengguna sudah login, arahkan mereka ke halaman lain (contoh: dashboard.html)
-        window.location.href = 'dashboard.html';
-    }
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
 
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        if (!gridCheck.checked) {
-            alert('Anda harus mengklik "Check me out" untuk melanjutkan.');
-            return;
-        }
-
-        const email = emailInput.value;
-        const password = passwordInput.value;
-
-        // Ganti URL API dengan URL mock API yang sesuai
-        const apiURL = 'https://mockapi.example.com/login';
-
-        fetch(apiURL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        })
-        .then(response => {
-            if (response.status === 200) {
-                // Login berhasil, simpan token atau informasi login di local storage
-                localStorage.setItem('token', 'your_token_here');
-
-                // Arahkan pengguna ke halaman lain (contoh: dashboard.html)
-                window.location.href = './index.html';
-            } else {
-                // Login gagal, Anda dapat menampilkan pesan kesalahan kepada pengguna.
-                alert('Login gagal. Coba lagi.');
-            }
-        })
-        .catch(error => {
-            console.error('Terjadi kesalahan:', error);
-        });
-    });
-
-    forgotPasswordLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        // Menampilkan alert atau arahkan ke halaman pemulihan kata sandi di sini
-        alert('Jika Anda lupa kata sandi, ikuti tautan pemulihan kata sandi.');
-    });
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const data = await validateEmail(email)
+    const isPasswordTrue = validatePassword(password, data[0])
+    
+    
+    
 });
+
+async function validateEmail(email) {
+    try {
+        const response = await fetch(`${url}?email=${email}`)
+        const data = await response.json()
+        console.log(data)
+        return data 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function validatePassword(password, data) {
+    return password === data.password
+}
+
+function saveRegistrationStatus(email) {
+
+    const registrationStatus = {
+        email,
+        registered: true,
+    }; 
+    if(isPasswordTrue) {
+        localStorage.setItem('loggedInUser', JSON.stringify(data[0]));
+        alert('login berhasil')
+    } else {
+        alert('Login gagal')
+}}
